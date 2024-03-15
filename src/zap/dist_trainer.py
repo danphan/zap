5eff62ed79171d15b8fd32cdd9fefd6ca56e8978
danphan <danphan2884@gmail.com>
2024-03-14 19:05:16 -0700

    Add loss tracker for validation

--- src/zap/dist_trainer.py
+++ src/zap/dist_trainer.py
@@ -1,4 +1,5 @@
 from typing import Optional, Any, Dict, Callable, List
+import os
 from dataclasses import dataclass
 from tqdm import tqdm
 from collections import defaultdict
@@ -11,24 +12,23 @@ from torch.optim.lr_scheduler import LambdaLR
 from zap.callbacks import Callback
 from zap.autoclip import AutoClip
 from torchmetrics import Metric
-from zap.metrics import LossTracker
+import torch.nn.parallel.DistributedDataParallel as DDP
 
 @dataclass
 class TrainerState:
     epoch : int = 0
     global_step : int = 0
 
-class Trainer:
+class DistTrainer:
     def __init__(
         self, 
         model : nn.Module, 
         train_dataloader : DataLoader,
         val_dataloader : DataLoader,
-        val_metrics : Dict[str, Metric],
+        val_metrics : Optional[Dict[str, Metric]] = None,
         #val_metrics : Dict[str, Callable],
         optimizer : Optimizer,
         use_amp : bool = True,
-        device : str = "cuda",
         autoclip_percentile : Optional[float] = 10.0,
         callbacks : Optional[List[Callback]] = None,
     ) -> None:
@@ -55,9 +55,13 @@ class Trainer:
 
         # Model
         self.model = model
-        self.device = device
-        self.device_type = device.split(":")[0] # "cuda" or "cpu"
-        self.model.to(device)
+        self.device = int(os.environ["LOCAL_RANK"])
+        self.model.to(self.device)
+        self.model = DDP(self.model, device_ids = [self.device])
+
+        # Parallelism setup
+        self.rank = int(os.environ["RANK"])
+        self.is_master_process = (self.rank == 0)
 
         # Training/validation data
         self.train_dataloader = train_dataloader
@@ -71,7 +75,10 @@ class Trainer:
         self.scaler = torch.cuda.amp.GradScaler(enabled = use_amp)
 
         ## Validation metrics/logging
-        self.val_metrics = {metric_name : metric.to(self.device) for metric_name, metric in val_metrics.items()}
+        if val_metrics is not None:
+            self.val_metrics = {metric_name : metric.to(self.device) for metric_name, metric in val_metrics.items()}
+        else:
+            self.val_metrics = None
 
         # For gradient clipping
         self.autoclipper = AutoClip(percentile=autoclip_percentile)
@@ -81,8 +88,6 @@ class Trainer:
 
         # Set up trainer state
         self.trainer_state = TrainerState() # initialize epoch and global_step to 0.
-
-        self.loss_tracker = LossTracker(self.model)
 
     def run_callbacks(self, callback_method : str, *args, **kwargs):
         """
@@ -109,9 +114,10 @@ class Trainer:
             self.run_callbacks("on_epoch_end", self)
 
             # Validate
-            val_metrics = self.evaluate()
+            if self.val_metrics is not None:
+                val_metrics = self.evaluate()
 
-            self.run_callbacks("on_validation_end", self, val_metrics)
+                self.run_callbacks("on_validation_end", self, val_metrics)
 
             self.trainer_state.epoch += 1
 
@@ -123,7 +129,7 @@ class Trainer:
         batch = {k : v.to(self.device) for k, v in batch.items()}
 
         # Forward pass
-        with torch.autocast(device_type = self.device_type, dtype = torch.float16):
+        with torch.autocast(device_type = "cuda", dtype = torch.float16):
             losses : Dict[str, torch.Tensor] = self.model(**batch)
             total_loss = sum(losses.values())
             losses.update(total_loss = total_loss)
@@ -159,15 +165,20 @@ class Trainer:
             "total_loss",
         )
         print(description)
-        pbar = tqdm(enumerate(self.train_dataloader), total = len(self.train_dataloader))
+        pbar = tqdm(enumerate(self.train_dataloader), total = len(self.train_dataloader), disable = not self.is_master_process)
         for batch_idx, batch in pbar:
             losses = self.train_one_step(batch, batch_idx)
+
 
             self.run_callbacks("on_step_end", self, losses)
 
             self.trainer_state.global_step += 1
 
             mem = f"{torch.cuda.memory_reserved(device = self.device)/1e9 if torch.cuda.is_available() else 0:.3g}G" # GB
+
+            # Logging only looks at the losses of the master process.
+            # We could reduce the losses to get the appropriate avg loss on the master process, but it's not worth it
+            # (the appropriately averaged loss would be the same order of magnitude, so who cares?)
             pbar.set_description(
                 ("{:20s}" + "{:20s}" + "{:<20.3g}"*len(losses.values())).format(
                     f"{self.trainer_state.epoch + 1}/{num_epochs}",
@@ -183,10 +194,18 @@ class Trainer:
         """
         self.model.eval()
 
-        num_metrics = len(self.model.loss_names) + len(self.val_metrics)
+        # Set up dictionary of metric values
+        val_losses = {loss_name : 0. for loss_name in self.model.loss_names}
 
-        print(("{:20s}" * num_metrics).format(*self.model.loss_names, *self.val_metrics.keys()))
-        pbar = tqdm(self.val_dataloader)
+        num_metrics = len(val_losses) + len(metrics)
+
+        num_samples = 0
+
+        # Keep track of running parameters
+        val_loss_states = {loss_name : torch.zeros((2,), device = self.device) for loss_name in self.val_metrics.keys()}
+
+        print(("{:20s}" * num_metrics).format(*val_losses.keys(), *metrics.keys()))
+        pbar = tqdm(self.val_dataloader, disable = not self.is_master_process)
         for batch in pbar:
             batch = {k : v.to(self.device) for k, v in batch.items()}
 
@@ -195,21 +214,36 @@ class Trainer:
             batch_size = preds.shape[0]
             labels = batch["label"]
 
-            # Calculate running metric
-            running_metrics = {}
-            for name, metric in self.val_metrics.items():
-                metric.update(preds, labels)
-                running_metrics[name] = metric.compute()
+            batch_metrics = {metric_name : metric_fn(preds, labels) for metric_name, metric_fn in self.val_metrics.items()}
 
-            # Calculate running losses
-            self.loss_tracker.update(preds, labels)
-            running_losses = self.loss_tracker.compute()
+            batch_losses = self.model.loss(preds, labels)
 
+            # Reduce losses appropriately so that the master process has the appropriate value
+            for loss_name, loss in batch_losses.items():
+                torch.distributed.reduce(loss, 0, )
+                batch_losses[loss_name]
+                
+            batch_losses 
+
+
+            #for k, v in batch_metrics.items():
+            #    metrics[k] = (metrics[k] * num_samples + v * batch_size) / (num_samples + batch_size)
+
+            for k, v in batch_losses.items():
+                val_losses[k] = (val_losses[k] * num_samples + v * batch_size) / (num_samples + batch_size)
+
+            num_samples += batch_size
+
+            #pbar.set_description(
+            #    ("{:<20.3g}" * num_metrics).format(*val_losses.values(), *metrics.values())
+            #)
             pbar.set_description(
-                ("{:<20.3g}" * num_metrics).format(*running_losses.values(), *running_metrics.values())
+                ("{:<20.3g}" * num_metrics).format(*val_losses.values(), *batch_metrics.values())
             )
 
-        return running_metrics | running_losses
+        metrics = {metric_name : metric.compute() for metric_name, metric_fn in self.val_metrics.items()} | val_losses
+
+        return metrics
 
     def make_lr_finder_dataloader(
         self,
@@ -315,5 +349,3 @@ class Trainer:
         plt.ylabel("Loss")
         plt.savefig(plot_path)
 
-
-                
---
+++ src/zap/metrics.py
@@ -0,0 +1,44 @@
+from typing import Callable, Dict, List
+import torch
+import torch.distributed as dist
+from zap.dist import is_distributed
+
+class LossTracker:
+    """
+    A torchmetrics.Metric-like API for tracking the loss in a distributed setting.
+
+    Args:
+        loss_fn (Callable) : A Callable which takes in preds and labels and returns Dict[str,torch.Tensor],
+           (this should be model.loss)
+    """
+
+    def __init__(
+        self, 
+        model,
+    ):
+        self.model = model
+        self.total_loss = {name : torch.tensor(0.) for name in self.model.loss_names}
+        self.num_samples = 0
+
+    def update(self, preds : torch.Tensor, labels : torch.Tensor) -> None:
+        """
+        Updates self.total_loss and self.num_samples
+        """
+        self.total_loss = {name : loss.to(preds.device) for name, loss in self.total_loss.items()}
+
+        batch_losses = self.model.loss(preds, labels)
+        batch_size = preds.shape[0]
+
+        for name in self.model.loss_names:
+            if is_distributed():
+                # Add all losses (use all_reduce just in case)
+                dist.all_reduce(batch_losses[name], 0, dist.ReduceOp.SUM)
+
+            self.total_loss[name] += batch_losses[name] * batch_size # unnormalize the loss, since it is defined per-sample
+
+        self.num_samples += batch_size
+
+    def compute(self) -> Dict[str, torch.Tensor]:
+        return {name : loss / self.num_samples for name, loss in self.total_loss.items()}
+            
+            
